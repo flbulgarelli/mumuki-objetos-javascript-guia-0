ee07d8b580ca7c636155823f6e94b19ae427d9ab
aguspina <aguspina87@gmail.com>
2015-07-15 19:56:54 -0300

    Change assert logic

--- 00100_Creando a Pepita/test.js
+++ 00100_Creando a Pepita/test.js
@@ -1,6 +1,6 @@
 describe("pepita", function(){
   it( "existe", function(){
-    assert.notOk(typeof(pepita) !== undefined );
+    assert(typeof(pepita) !== undefined );
   });
   it("es un objecto", function(){
     assert(pepita.constructor == Object);
