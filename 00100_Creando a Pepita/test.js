d36ad59ee0b56cb51ba40210ce157c150b3e669b
aguspina <aguspina87@gmail.com>
2015-07-15 19:55:43 -0300

    Remove unused code

--- 00100_Creando a Pepita/test.js
+++ 00100_Creando a Pepita/test.js
@@ -1,12 +1,3 @@
-describe "pepita" do
-  it('existe') do
-      expect(!!defined? pepita).to be true
-  end
-  it('es un objeto') do
-      expect(pepita.class).to be Object
-  end
-end
-
 describe("pepita", function(){
   it( "existe", function(){
     assert.notOk(typeof(pepita) !== undefined );
