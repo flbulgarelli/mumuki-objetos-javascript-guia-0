2dd6c267bec9fd444b6fa751322320b4b247d911
aguspina <aguspina87@gmail.com>
2015-07-15 22:06:07 -0300

    Remove unused code

--- 00140_Métodos, Segunda Parte/test.js
+++ 00140_Métodos, Segunda Parte/test.js
@@ -1,15 +1,3 @@
-describe "anastasia" do
-  it('existe') do
-      expect(!!defined? anastasia).to be true
-  end
-  it('es un objeto') do
-      expect(anastasia.class).to be Object
-  end
-  it('anasitasia.cantar! devuelve "pri"') do
-      expect(anastasia.cantar!).to eq "pri"
-  end
-end
-
 describe("anastasia", function(){
   it( "existe", function(){
     assert(typeof(anastasia) !== undefined );
