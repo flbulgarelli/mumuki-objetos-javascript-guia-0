89650af2cb279cadd3edeba50413aa98f1d13638
aguspina <aguspina87@gmail.com>
2015-07-15 23:01:33 -0300

    Remove unused code

--- 00170_Responsabilidades: una vuelta de tuerca/test.js
+++ 00170_Responsabilidades: una vuelta de tuerca/test.js
@@ -1,12 +1,3 @@
-describe "" do
-  it('pepita entiende vivacidad') do
-      expect(pepita.respond_to? :vivacidad).to be true
-  end
-  it('la vivacidad de pepita es 15') do
-      expect(pepita.vivacidad).to eq 15
-  end
-end
-
 describe('', function () {
   it('pepita entiende vivacidad', function () {
     assert(pepita.vivacidad() !== undefined ); 
