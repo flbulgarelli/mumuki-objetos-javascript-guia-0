8339ab6a16c4f08a75e60670160f95a4dfd31803
aguspina <aguspina87@gmail.com>
2015-07-15 22:11:14 -0300

    Remove unused code

--- 00150_Más Mensajes/test.js
+++ 00150_Más Mensajes/test.js
@@ -1,9 +1,3 @@
-describe "anastasia" do
-  it('se le envio el mensaje cantar exactamente una vez') do
-      expect(anastasia.canto).to eq 1
-  end
-end
-
 describe('anastasia', function () {
   it('se le envio el mensaje cantar exactamente una vez', function () {
     assert(anastasia.canto === 1);
